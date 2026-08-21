51bb9d49556614f541c3bb778f8b87fa4ea90294
lutfyt33 <lutfyt33@gmail.com>
2026-08-21 20:34:34 +0700

    Create data.js

---
+++ Js/data.js
@@ -0,0 +1,19 @@
+// data.js - DATA TOMBOL & LINK PROYEK
+const PROJECTS = [
+    { id: 1, name: 'Project Alpha', url: 'https://github.com' },
+    { id: 2, name: 'Project Beta', url: 'https://youtube.com' },
+    { id: 3, name: 'Project Gamma', url: 'https://twitter.com' },
+    { id: 4, name: 'Project Delta', url: 'https://instagram.com' },
+    { id: 5, name: 'Project Epsilon', url: 'https://tiktok.com' },
+    { id: 6, name: 'Project Zeta', url: 'https://discord.com' },
+    { id: 7, name: 'Project Eta', url: 'https://spotify.com' },
+    { id: 8, name: 'Project Theta', url: 'https://reddit.com' },
+    { id: 9, name: 'Project Iota', url: 'https://linkedin.com' },
+    { id: 10, name: 'Project Kappa', url: 'https://facebook.com' },
+    // tambahin sendiri kalo mau lebih
+];
+
+// GANTI URL DI SINI UNTUK MENGUBAH LINK TOMBOL
+// Contoh: { id: 1, name: 'Website Saya', url: 'https://website-saya.com' }
+
+export default PROJECTS;
