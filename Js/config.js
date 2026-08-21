ea48332149c85db867983e13cb6631271b04870e
lutfyt33 <lutfyt33@gmail.com>
2026-08-21 20:34:07 +0700

    Create config.js

---
+++ Js/config.js
@@ -0,0 +1,11 @@
+// config.js
+const CONFIG = {
+    appName: 'APEX OFFICIAL',
+    version: '1.0.0',
+    loadingDuration: 2500,
+    glitchDuration: 1000,
+    progressStep: 2,
+    progressInterval: 70,
+};
+
+export default CONFIG;
